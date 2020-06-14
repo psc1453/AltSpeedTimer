dba99937b5394e4391a24bc94ce887cd0bdd2948
psc1453 <pscpeng@outlook.com>
2020-06-14 15:12:05 +0800

    Initial Commit

---
+++ AltSpeedTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AltSpeedTimer
+//
+//  Created by 彭世辰 on 2020/6/14.
+//  Copyright © 2020 Peng Shichen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
